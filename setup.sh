a852ce2146401b92053466e7e6e82e4f219da25f
Dustin <dzubke@outlook.com>
2020-01-20 21:48:51 +0000

    udpated to pyt_update

--- setup.sh
+++ setup.sh
@@ -2,6 +2,8 @@
 # Run `source setup.sh` from this directory.
 export PYTHONPATH=`pwd`:`pwd`/libs/warp-ctc/pytorch_binding:`pwd`/libs:$PYTHONPATH
 
+export PYTHONPATH=`pwd`:`pwd`/libs/warp-ctc/:`pwd`/libs:$PYTHONPATH
+
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:`pwd`/libs/warp-ctc/build
 
 #export PYTHONPATH=$PYTHONPATH:/Users/dustin/CS/consulting/firstlayerai/phoneme_classification/src/awni_speech/speech
