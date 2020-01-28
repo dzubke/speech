f7eae7f0f9b81cf472e8be022b4f4a6bb453d2b5
Dustin <dzubke@outlook.com>
2020-01-28 18:00:20 +0000

    centralized import_export

--- onnx_coreml/onnx_to_coreml.py
+++ onnx_coreml/onnx_to_coreml.py
@@ -1,54 +1,14 @@
-import sys
 import argparse
 
-from onnx import onnx_pb
-from onnx_coreml import convert
-import coremltools
 from get_paths import onnx_coreml_paths
+from import_export import onnx_coreml_export, onnx_coreml_export_2
 
-def converter_1(onnx_path, coreml_path):
-    print("mark 1")
-    model_file = open(onnx_path, 'rb')
-    print("mark 2")
-    model_proto = onnx_pb.ModelProto()
-    print("mark 3")
-    model_proto.ParseFromString(model_file.read())
-    print("mark 4")
-    coreml_model = convert(
-        model_proto, 
-        minimum_ios_deployment_target = '13' 
-    )
-    print("mark 5")
-    coreml_model.save(coreml_path)
-
-def converter_2(model_in, model_out):
-    mlmodel = convert(model=model_in, minimum_ios_deployment_target='13')
-      
-    # rename inputs/outputs
-    spec = mlmodel.get_spec()
-    coremltools.utils.rename_feature(spec, current_name='input.1', new_name='input_tensor')
-    coremltools.utils.rename_feature(spec, current_name='12', new_name='network_output')
-    mlmodel = coremltools.models.MLModel(spec)
-
-    # Save converted CoreML model
-    mlmodel.save(model_out)
-
-
-def generate_input_output_names(model_name):
-    if model_name == "super_resolution":
-        return ['input'], ['output']
-    elif model_name == 'resnet18':
-        return ['input'], ['output']
-    else: 
-        return ['inputs'], ['outputs']
 
 
 def main(model_name):
     onnx_path, coreml_path = onnx_coreml_paths(model_name)
-    #input_names, output_names = generate_input_output_names(model_name)
-    converter_1(onnx_path, coreml_path)#, input_names, output_names)
-    #converter_2(onnx_path, coreml_path)
-
+    onnx_coreml_export(onnx_path, coreml_path)
+    #onnx_coreml_export_2(onnx_path, coreml_path)
 
 if __name__ == "__main__":
     # command format  python onnx_to_coreml.py <model_name>
--- onnx_coreml/torch_to_onnx.py
+++ onnx_coreml/torch_to_onnx.py
@@ -3,22 +3,23 @@ import argparse
 
 from get_paths import pytorch_onnx_paths
 from get_test_input import generate_test_input
+from import_export import torch_load, torch_onnx_export
+
 
 def onnx_export(model_name):
 
-    pytorch_path, onnx_path = pytorch_onnx_paths(model_name)
+    torch_path, onnx_path = pytorch_onnx_paths(model_name)
     
     torch_device = 'cpu'
 
-    torch_model = torch.load(pytorch_path, map_location=torch.device(torch_device))
+    torch_model = torch_load(torch_path, torch_device)
     torch_model.eval()    
     #dummy_tensor = (torch.from_numpy(dummy_input[0][0]).float().to('cpu'), [27])
     #dummy_tensor = [torch.FloatTensor(1, 200, 161), torch.IntTensor(1, 41)]
     
     input_tensor = generate_test_input("pytorch", model_name, set_device=torch_device) 
-    onnx_model = torch.onnx.export(
-        torch_model, input_tensor, onnx_path, input_names=['data'],
-        output_names = ['output'], verbose=True
+    torch_onnx_export(
+        torch_model, input_tensor, onnx_path, verbose=True
         )
 
 def main(model_name):
