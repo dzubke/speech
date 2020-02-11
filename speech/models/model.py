37a9ec1dad7612ec2708c5cda0a8d79f8df09d5d
Dustin <dzubke@outlook.com>
2020-02-11 15:30:56 +0000

    20200207 model from phoneme-1

--- speech/models/model.py
+++ speech/models/model.py
@@ -80,7 +80,7 @@ class Model(nn.Module):
         """
         raise NotImplementedError
 
-    def encode(self, x, rnn_args):
+    def encode(self, x, rnn_args=None):
         """this function processes the input data x through the CNN and RNN layers specified
             in the model encoder config.
 
