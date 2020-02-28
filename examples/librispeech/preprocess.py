b61c6a3f4ad6186eb1d626710166f2dfc10c1014
Dustin Zubke <dzubke@outlook.com>
2020-02-28 08:32:44 -0700

    updated preprocessing scripts

--- examples/librispeech/preprocess.py
+++ examples/librispeech/preprocess.py
@@ -37,9 +37,10 @@ def main(output_directory, use_phonemes):
 
 
 def build_json(path, use_phonemes):
-    transcripts = load_transcripts(path, use_phonemes)
+    transcripts, unknown_words = load_transcripts(path, use_phonemes)
     dirname = os.path.dirname(path)
     basename = os.path.basename(path) + os.path.extsep + "json"
+    unk_words_fname = "libsp_"+os.path.basename(path)+"_unk_words.txt"
     with open(os.path.join(dirname, basename), 'w') as fid:
         for file_key, text in tqdm.tqdm(transcripts.items()):
             wave_file = path_from_key(file_key, path, ext="wav")
@@ -50,27 +51,30 @@ def build_json(path, use_phonemes):
             json.dump(datum, fid)
             fid.write("\n")
 
+    with open(unk_words_fname, 'w') as fid:
+        for word in unknown_words:
+            fid.write(word+'\n')
 
 def load_transcripts(path, use_phonemes=True):
     pattern = os.path.join(path, "*/*/*.trans.txt")
     files = glob.glob(pattern)
     data = {}
+    unknown_words=set()
     if use_phonemes: 
-        word_phoneme_dict = lexicon_to_dict()
+        word_phoneme_dict = lexicon_to_dict('librispeech')
         #save_lexicon_to_dict()
 
     for f in tqdm.tqdm(files):
         with open(f) as fid:
             lines = (l.strip().lower().split() for l in fid)
             if use_phonemes: 
-                lines = (
-                    (l[0], phones for word in l[1:] 
-                                    for phones in word_phoneme_dict[word]) 
-                                        for l in lines)
+                lines = ((l[0], transcript_to_phonemes(l[1:])) for l in lines)
+                unk_words = filter(lambda x: word_phoneme_dict[x] == "unk", [word for l in lines for word in l[1:]])
             else: 
                 lines = ((l[0], " ".join(l[1:])) for l in lines)
             data.update(lines)
-    return data
+            unknown_words.update(set(unk_words))
+    return data, unknown_words
 
 
 def transcript_to_phonemes(words):
@@ -80,7 +84,6 @@ def transcript_to_phonemes(words):
     for word in words:
         phonemes.extend(word_phoneme_dict[word])
     return phonemes
-
 
 def path_from_key(key, prefix, ext):
     dirs = key.split("-")
@@ -93,54 +96,7 @@ def convert_to_wav(path):
     data_helpers.convert_full_set(path, "*/*/*/*.flac")
 
 
-def lexicon_to_dict():
-    """This function reads the librispeech-lexicon.txt file which is a mapping of words in the
-        librispeech corpus to phoneme labels and represents the file as a dictionary.
-        The digit accents are removed from the file name. 
-        Note: the librispeech-lexicon.txt file needs to be in the same directory as this file.
-    """
-    
-    lex_dict = defaultdict(lambda: "unk")
-    with open("librispeech-lexicon.txt", 'r') as fid:
-        lexicon = (l.strip().lower().split() for l in fid)
-        for line in lexicon: 
-            word = line[0]
-            phones = line[1:]
-            # remove the accent digit from the phone, string.digits = '0123456789'
-            phones = list(map(lambda x: x.rstrip(string.digits), phones))
-            # the if-statement will ignore the second pronunciation (phone list)
-            if lex_dict[word] == "unk":
-                lex_dict[word] = phones
 
-    return lex_dict
-
-
-def check_phones():
-    """This function compares the phonemes in the librispeech corpus with the phoneme labels in the 39-phonemes
-    in the timit dataset outlined here: 
-    https://www.semanticscholar.org/paper/Speaker-independent-phone-recognition-using-hidden-Lee-Hon/3034afcd45fc190ed71982828b77f6e4154bdc5c
-    
-    Discrepencies in the CMU-39 and timit-39 phoneme sets and the librispeech phonemes: 
-     - included in CMU-39 but not timit-39:  ao, zh, 
-     - included timit-39 but not CMU-39: dx, sil
-    """
-    # standard 39 phones in the timit used by awni dictionary
-    timit_phones39 = set(['ae', 'ah', 'aa', 'aw', 'er', 'ay', 'b', 'ch', 'd', 'dh', 'dx', 'eh', 'l', 'm', 'n', 'ng', 'ey', 'f', 'g', 'hh', 'ih', 'iy', 'jh', 'k', 'l', 'ow', 'oy', 'p', 'r', 's', 'sh', 't', 'th', 'uh', 'uw', 'v', 'w', 'y', 'z', 'sil'])
-    cmu_phones = set(['aa', 'ae', 'ah', 'ao', 'aw', 'ay',  'b', 'ch', 'd', 'dh', 'eh', 'er', 'ey', 'f', 'g', 'hh', 'ih', 'iy', 'jh', 'k', 'l', 'm', 'n', 'ng', 'ow', 'oy', 'p', 'r', 's', 'sh', 't', 'th', 'uh', 'uw', 'v', 'w', 'y', 'z', 'zh'])
-    print(f"length of timit_dict: {len(timit_phones39)}")
-    librispeech_phones = set()
-    
-    # greating a set of the librispeech phones by looping over every phone list in the word_to_phoneme mapping
-    for phones in word_phoneme_dict.values():
-        # looping over every phone in the word pronunciation
-        for phone in phones:
-            if phone not in librispeech_phones:
-                librispeech_phones.add(phone)
-
-    print(f"phones in librispeech but not cmu: {librispeech_phones.difference(cmu_phones)}")
-    print(f"phones in cmu but not librispeech: {cmu_phones.difference(librispeech_phones)}")
-    print(f"phones in timit but not cmu: {timit_phones39.difference(cmu_phones)}")
-    print(f"phones in cmubut not timit: {cmu_phones.difference(timit_phones39)}")
 
 
 if __name__ == "__main__":
