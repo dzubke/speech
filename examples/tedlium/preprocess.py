dbcd5d6b72981e8b1ff8b460b412c3d6cbd0cf17
Dustin Zubke <dzubke@outlook.com>
2020-03-02 13:05:48 -0700

    update to preprocess in libsp

--- examples/tedlium/preprocess.py
+++ examples/tedlium/preprocess.py
@@ -1,47 +1,88 @@
+# standard libraries
 import os
 import re
 import argparse
-
-import wget
-import tarfile
 import subprocess
-import unicodedata
 import io
-#from utils import create_manifest
-from tqdm import tqdm
 from collections import defaultdict
 
-def lexicon_to_dict(ted_dir):
-    """
-    """
-    lexicon_path = os.path.join(ted_dir, "TEDLIUM.152k.dic")
-    
-    lex_dict = defaultdict(lambda: "unk")
-    with open(lexicon_path, 'r') as fid:
-        lexicon = (l.strip().lower().split() for l in fid)
-        for line in lexicon: 
-            word = line[0]
-            phones = line[1:]
-            # remove the accent digit from the phone, string.digits = '0123456789'
-            # phones = list(map(lambda x: x.rstrip(string.digits), phones))
-            # the if-statement will ignore the second pronunciation (phone list)
-            if lex_dict[word] != "unk":
-                print(f"repated word: {word}")
-            lex_dict[word] = phones 
-    
-    lex_dict = defaultdict(lambda: "unk", 
-                {key: value for key, value in lex_dict.items() if not re.search("\(\d\)$", key)})
 
-    return lex_dict
+# third-party libraries
+import wget
+import tarfile
+import unicodedata
+#from utils import create_manifest
+from tqdm import tqdm
 
-def _preprocess_transcript(phrase):
-    return phrase.strip().upper()
+#project libraries
+from speech.utils import data_helpers
+
+def main(target_dir, tar_path, sample_rate, min_duration, max_duration, use_phonemes):
+    target_dl_dir = target_dir
+    if not os.path.exists(target_dl_dir):
+        os.makedirs(target_dl_dir)
+
+    target_unpacked_dir = os.path.join(target_dl_dir, "TEDLIUM_release-3")
+    if tar_path and os.path.exists(tar_path):
+        target_file = tar_path
+    else:
+        print("Could not find downloaded TEDLIUM archive, Downloading corpus...")
+        #wget.download(TED_LIUM_V2_DL_URL, target_dl_dir)
+        target_file = os.path.join(target_dl_dir, "TEDLIUM_release-3.tgz")
+
+    if not os.path.exists(target_unpacked_dir):
+        print("Unpacking corpus...")
+        tar = tarfile.open(target_file)
+        tar.close()
+        tar.extractall(target_dl_dir)
+    else:
+        print("Found TEDLIUM directory, skipping unpacking of tar files")
+
+    train_ted_dir = os.path.join(target_unpacked_dir, "data")
+    val_ted_dir = os.path.join(target_unpacked_dir, "legacy", "dev")
+    test_ted_dir = os.path.join(target_unpacked_dir, "legacy", "test")
+
+    prepare_dir(train_ted_dir, use_phonemes)
+    prepare_dir(val_ted_dir,  use_phonemes)
+    prepare_dir(test_ted_dir,  use_phonemes)
+    #print('Creating manifests...')
+
+    #create_manifest(train_ted_dir, 'ted_train_manifest.csv', min_duration, max_duration)
+    #create_manifest(val_ted_dir, 'ted_val_manifest.csv')
+    #create_manifest(test_ted_dir, 'ted_test_manifest.csv')
 
 
-def cut_utterance(src_sph_file, target_wav_file, start_time, end_time, sample_rate=16000):
-    subprocess.call(["sox {}  -r {} -b 16 -c 1 {} trim {} ={}".format(src_sph_file, str(sample_rate),
-                                                                      target_wav_file, start_time, end_time)],
-                    shell=True)
+def prepare_dir(ted_dir, use_phonemes):
+    converted_dir = os.path.join(ted_dir, "converted")
+    # directories to store converted wav files and their transcriptions
+    wav_dir = os.path.join(converted_dir, "wav")
+    if not os.path.exists(wav_dir):
+        os.makedirs(wav_dir)
+    txt_dir = os.path.join(converted_dir, "txt")
+    if not os.path.exists(txt_dir):
+        os.makedirs(txt_dir)
+    counter = 0
+    entries = os.listdir(os.path.join(ted_dir, "sph"))
+    for sph_file in tqdm(entries, total=len(entries)):
+        speaker_name = sph_file.split('.sph')[0]
+
+        sph_file_full = os.path.join(ted_dir, "sph", sph_file)
+        stm_file_full = os.path.join(ted_dir, "stm", "{}.stm".format(speaker_name))
+
+        assert os.path.exists(sph_file_full) and os.path.exists(stm_file_full)
+        if use_phonemes:
+            LEXICON_PATH = "TEDLIUM.152k.dic"
+            word_phoneme_dict = data_helpers.lexicon_to_dict(LEXICON_PATH, corpus_name="tedlium")
+        all_utterances = get_utterances_from_stm(stm_file_full)
+
+        all_utterances = filter(filter_short_utterances, all_utterances)
+        for utterance_id, utterance in enumerate(all_utterances):
+            target_wav_file = os.path.join(wav_dir, "{}_{}.wav".format(utterance["filename"], str(utterance_id)))
+            target_txt_file = os.path.join(txt_dir, "{}_{}.txt".format(utterance["filename"], str(utterance_id)))
+            cut_utterance(sph_file_full, target_wav_file, utterance["start_time"], utterance["end_time"])
+            with io.FileIO(target_txt_file, "w") as f:
+                f.write(_preprocess_transcript(utterance["transcript"], use_phonemes, word_phoneme_dict))
+        counter += 1
 
 
 def get_utterances_from_stm(stm_file):
@@ -65,6 +106,7 @@ def get_utterances_from_stm(stm_file):
                     "start_time": start_time, "end_time": end_time,
                     "filename": filename, "transcript": transcript
                 })
+            
         return res
 
 
@@ -72,70 +114,51 @@ def filter_short_utterances(utterance_info, min_len_sec=1.0):
     return (utterance_info["end_time"] - utterance_info["start_time"]) > min_len_sec
 
 
-def prepare_dir(ted_dir):
-    converted_dir = os.path.join(ted_dir, "converted")
-    # directories to store converted wav files and their transcriptions
-    wav_dir = os.path.join(converted_dir, "wav")
-    if not os.path.exists(wav_dir):
-        os.makedirs(wav_dir)
-    txt_dir = os.path.join(converted_dir, "txt")
-    if not os.path.exists(txt_dir):
-        os.makedirs(txt_dir)
-    counter = 0
-    entries = os.listdir(os.path.join(ted_dir, "sph"))
-    for sph_file in tqdm(entries, total=len(entries)):
-        speaker_name = sph_file.split('.sph')[0]
-
-        sph_file_full = os.path.join(ted_dir, "sph", sph_file)
-        stm_file_full = os.path.join(ted_dir, "stm", "{}.stm".format(speaker_name))
-
-        assert os.path.exists(sph_file_full) and os.path.exists(stm_file_full)
-        all_utterances = get_utterances_from_stm(stm_file_full)
-
-        all_utterances = filter(filter_short_utterances, all_utterances)
-        for utterance_id, utterance in enumerate(all_utterances):
-            target_wav_file = os.path.join(wav_dir, "{}_{}.wav".format(utterance["filename"], str(utterance_id)))
-            target_txt_file = os.path.join(txt_dir, "{}_{}.txt".format(utterance["filename"], str(utterance_id)))
-            cut_utterance(sph_file_full, target_wav_file, utterance["start_time"], utterance["end_time"],
-                          sample_rate=sample_rate)
-            with io.FileIO(target_txt_file, "w") as f:
-                f.write(_preprocess_transcript(utterance["transcript"]).encode('utf-8'))
-        counter += 1
+def cut_utterance(src_sph_file, target_wav_file, start_time, end_time, sample_rate=16000):
+    subprocess.call(["sox {}  -r {} -b 16 -c 1 {} trim {} ={}".format(src_sph_file, str(sample_rate),
+                                                                      target_wav_file, start_time, end_time)],
+                    shell=True)
 
 
-def main(target_dir, tar_path, sample_rate, min_duration, max_duration):
-    target_dl_dir = target_dir
-    if not os.path.exists(target_dl_dir):
-        os.makedirs(target_dl_dir)
+def _preprocess_transcript(phrase, use_phonemes, word_phoneme_dict):
+    phrase = phrase.strip()
+    if use_phonemes:
+        phonemes = []
+        for word in phrase:
+            print(word_phoneme_dict[word])
+            phonemes.extend(word_phoneme_dict[word])
+        print(phonemes)
+        phrase = phonemes
+    
+    return phrase
 
-    target_unpacked_dir = os.path.join(target_dl_dir, "TEDLIUM_release-3")
-    if tar_path and os.path.exists(tar_path):
-        target_file = tar_path
-    else:
-        print("Could not find downloaded TEDLIUM archive, Downloading corpus...")
-        #wget.download(TED_LIUM_V2_DL_URL, target_dl_dir)
-        target_file = os.path.join(target_dl_dir, "TEDLIUM_release-3.tgz")
 
-    if not os.path.exists(target_unpacked_dir):
-        print("Unpacking corpus...")
-        tar = tarfile.open(target_file)
-        tar.close()
-        tar.extractall(target_dl_dir)
-    else:
-        print("Found TEDLIUM directory, skipping unpacking of tar files")
 
-    train_ted_dir = os.path.join(target_unpacked_dir, "train")
-    val_ted_dir = os.path.join(target_unpacked_dir, "dev")
-    test_ted_dir = os.path.join(target_unpacked_dir, "test")
 
-    prepare_dir(train_ted_dir)
-    prepare_dir(val_ted_dir)
-    prepare_dir(test_ted_dir)
-    #print('Creating manifests...')
+# def lexicon_to_dict(ted_dir):
+#     """
+#     """
+#     lexicon_path = os.path.join(ted_dir, "TEDLIUM.152k.dic")
+    
+#     lex_dict = defaultdict(lambda: "unk")
+#     with open(lexicon_path, 'r') as fid:
+#         lexicon = (l.strip().lower().split() for l in fid)
+#         for line in lexicon: 
+#             word = line[0]
+#             phones = line[1:]
+#             # remove the accent digit from the phone, string.digits = '0123456789'
+#             # phones = list(map(lambda x: x.rstrip(string.digits), phones))
+#             # the if-statement will ignore the second pronunciation (phone list)
+#             if lex_dict[word] != "unk":
+#                 print(f"repated word: {word}")
+#             lex_dict[word] = phones 
+    
+#     lex_dict = defaultdict(lambda: "unk", 
+#                 {key: value for key, value in lex_dict.items() if not re.search("\(\d\)$", key)})
 
-    #create_manifest(train_ted_dir, 'ted_train_manifest.csv', min_duration, max_duration)
-    #create_manifest(val_ted_dir, 'ted_val_manifest.csv')
-    #create_manifest(test_ted_dir, 'ted_test_manifest.csv')
+#     return lex_dict
+
+
 
 
 if __name__ == "__main__":
@@ -148,8 +171,10 @@ if __name__ == "__main__":
                         help='Prunes training samples shorter than the min duration (given in seconds, default 1)')
     parser.add_argument('--max-duration', default=15, type=int,
                         help='Prunes training samples longer than the max duration (given in seconds, default 15)')
+    parser.add_argument('--use_phonemes', default=True, type=bool,
+                        help='Determines whether output phoneme labels.')
     args = parser.parse_args()
 
     TED_LIUM_V2_DL_URL = "http://www.openslr.org/resources/19/TEDLIUM_release2.tar.gz"
 
-    main(args.target_dir, args.tar_path, args.sample_rate, args.min_duration, args.max_duration)
+    main(args.target_dir, args.tar_path, args.sample_rate, args.min_duration, args.max_duration, args.use_phonemes)
