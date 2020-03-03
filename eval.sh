0cd58bfdb9d61cae02ce6817f77b57b52ff93f4d
Dustin <dzubke@outlook.com>
2020-03-03 10:00:16 -0700

    20200303 model update

--- eval.sh
+++ eval.sh
@@ -3,7 +3,11 @@
 
 mkdir ./examples/librispeech/models/ctc_models/$1/$2
 cp ./examples/librispeech/models/ctc_models/$1/* ./examples/librispeech/models/ctc_models/$1/$2
+echo Evaluating the Speak Test Set
 python eval.py ./examples/librispeech/models/ctc_models/$1/$2 ~/awni_speech/data/speak_test_data/speak_test.json --save ./predictions/$1-$2_speak_test_predictions.json
+echo Evaluating Dustin Clean Testset
 python eval.py ./examples/librispeech/models/ctc_models/$1/$2 ~/awni_speech/data/dustin_test_data/20191202_clean/drz_test.json --save ./predictions/$1-$2_1202_predictions.json
+echo Evaluating the Dustin Noisy Testset
 python eval.py ./examples/librispeech/models/ctc_models/$1/$2 ~/awni_speech/data/dustin_test_data/20191118_plane/simple/drz_test.json --save ./predictions/$1-$2_1118-simple_predictions.json
+echo Evaluating Librispeech Combo Testset
 python eval.py ./examples/librispeech/models/ctc_models/$1/$2 ~/awni_speech/data/LibriSpeech/test-combo.json --save ./predictions/$1-$2_libsp-test-combo_predictions.json
