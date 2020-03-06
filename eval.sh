146717f2847d6cdd1c938b54accf0319af08bd66
Dustin <dzubke@outlook.com>
2020-03-06 08:32:20 -0700

    0306 new model

--- eval.sh
+++ eval.sh
@@ -3,11 +3,11 @@
 
 mkdir ./examples/librispeech/models/ctc_models/$1/$2
 cp ./examples/librispeech/models/ctc_models/$1/* ./examples/librispeech/models/ctc_models/$1/$2
-echo Evaluating the Speak Test Set
+echo -e "\nEvaluating the Speak Test Set"
 python eval.py ./examples/librispeech/models/ctc_models/$1/$2 ~/awni_speech/data/speak_test_data/speak_test.json --save ./predictions/$1-$2_speak_test_predictions.json
-echo Evaluating Dustin Clean Testset
+echo -e "\nEvaluating Dustin Clean Testset"
 python eval.py ./examples/librispeech/models/ctc_models/$1/$2 ~/awni_speech/data/dustin_test_data/20191202_clean/drz_test.json --save ./predictions/$1-$2_1202_predictions.json
-echo Evaluating the Dustin Noisy Testset
+echo -e "\nEvaluating the Dustin Noisy Testset"
 python eval.py ./examples/librispeech/models/ctc_models/$1/$2 ~/awni_speech/data/dustin_test_data/20191118_plane/simple/drz_test.json --save ./predictions/$1-$2_1118-simple_predictions.json
-echo Evaluating Librispeech Combo Testset
-python eval.py ./examples/librispeech/models/ctc_models/$1/$2 ~/awni_speech/data/LibriSpeech/test-combo.json --save ./predictions/$1-$2_libsp-test-combo_predictions.json
+echo -e "\nEvaluating Libsp Tedlium Testset"
+python eval.py ./examples/librispeech/models/ctc_models/$1/$2 ~/awni_speech/data/libsp_tedlium/test-libsp-tedlium.json --save ./predictions/$1-$2_test-libsp-tedlium_predictions.json
