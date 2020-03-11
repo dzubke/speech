a77c7496fac5e4299928c00a2604491573dde20a
Dustin Zubke <dzubke@outlook.com>
2020-03-11 11:17:38 -0600

    added model_debug and check_data in tests dir

---
+++ tests/check_data.py
@@ -0,0 +1,47 @@
+# standard library
+import json
+import argparse
+
+# project libraries
+from tests import model_debug
+from speech.utils.io import load
+
+
+
+def main(json_path, preproc_path):
+    """
+        this file iterates through all audio paths in json_path, processes the audio,
+        and checks if there are any nan values in the processed output.
+        Arguments:
+            json_path (str): path to data json
+            preproc_path (str): path to dir with preproc object, don't include preproc.pyc in path
+    """
+
+    with open(json_path) as fid:
+        data_json = [json.loads(l) for l in fid]
+
+    _, preproc = load(preproc_path)
+
+
+    json_has_nan = False
+    for sample in data_json:
+        samp_path = sample["audio"]
+        inputs, sample_rate = model_debug.load_audio(samp_path, preproc)
+
+        # checks for nan values
+        if (inputs!=inputs).any():
+            print(f"nan value in: {samp_path}")
+            json_has_nan = True
+    
+    if not json_has_nan:
+        print(f"data json doesn't have nan values")
+    
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Checks for nan values in processed audio specified in json_dir")
+    parser.add_argument("json_path", help="path to the data json to be checked for nan values")
+    parser.add_argument("preproc_path", help="path to directory with preproc object")
+    args = parser.parse_args()
+
+    main(args.json_path, args.preproc_path)
