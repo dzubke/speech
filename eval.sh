f78d6a6cfe0b40d95823bf8a5acaa2bac5a073af
Dustin <dzubke@outlook.com>
2020-04-08 08:56:53 -0600

    0408 model update from ph-3

--- eval.sh
+++ eval.sh
@@ -9,5 +9,5 @@ echo -e "\nEvaluating Dustin Clean Testset"
 python eval.py ./examples/librispeech/models/ctc_models/$1/$2 ~/awni_speech/data/dustin_test_data/20191202_clean/drz_test.json --save ./predictions/$1-$2_1202_predictions.json
 echo -e "\nEvaluating the Dustin Noisy Testset"
 python eval.py ./examples/librispeech/models/ctc_models/$1/$2 ~/awni_speech/data/dustin_test_data/20191118_plane/simple/drz_test.json --save ./predictions/$1-$2_1118-simple_predictions.json
-echo -e "\nEvaluating Libsp Tedlium Testset"
-python eval.py ./examples/librispeech/models/ctc_models/$1/$2 ~/awni_speech/data/libsp-tedlium/test-libsp-tedlium.json --save ./predictions/$1-$2_test-libsp-tedlium_predictions.json
+echo -e "\nEvaluating lib-ted-cv Testset"
+python eval.py ./examples/librispeech/models/ctc_models/$1/$2 ~/awni_speech/data/lib-ted-cv/test_lib-ted-cv.json --save ./predictions/$1-$2_test-lib-ted-cv_predictions.json
