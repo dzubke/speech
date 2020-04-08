54c5a2b88f2b2415b39743582e8840f76bdf64c6
Dustin Zubke <dzubke@outlook.com>
2020-04-08 13:14:42 -0600

    updated reprocess  noise_inject_test

---
+++ speech/dataset_info.py
@@ -0,0 +1,58 @@
+class Dataset():
+    
+    def __init__(self, corpus_name:str, dataset_name:str, audio_dir:str, pattern:str):
+        self.corpus_name = corpus_name
+        self.dataset_name = dataset_name
+        self.audio_dir = audio_dir
+        self.pattern = pattern
+
+
+class Librispeech100Dataset(Dataset):
+    def __init__(self):
+        self.corpus_name = "librispeech"
+        self.dataset_name = "train-clean-100"
+        self.audio_dir = "/home/dzubke/awni_speech/data/LibriSpeech/train-clean-100/"
+        self.pattern = "*/*/*.wav"
+
+
+class Librispeech360Dataset(Dataset):
+    def __init__(self):
+        self.corpus_name = "librispeech"
+        self.dataset_name = "train-clean-360"
+        self.audio_dir = "/home/dzubke/awni_speech/data/LibriSpeech/train-clean-360/"
+        self.pattern = "*/*/*.wav"
+
+class Librispeech500Dataset(Dataset):
+    def __init__(self):
+        self.corpus_name = "librispeech"
+        self.dataset_name = "train-other-500"
+        self.audio_dir = "/home/dzubke/awni_speech/data/LibriSpeech/train-clean-500/"
+        self.pattern = "*/*/*.wav"
+
+class CommonvoiceDataset(Dataset):
+    def __init__(self):
+        self.corpus_name = "common-voice"
+        self.dataset_name = "common-voice"
+        self.audio_dir = "/home/dzubke/awni_speech/data/common-voice/clips/"
+        self.pattern = "*.wav"
+
+class TedliumDataset(Dataset):
+    def __init__(self):
+        self.corpus_name = "tedlium"
+        self.dataset_name = "tedlium"
+        self.audio_dir = "/home/dzubke/awni_speech/data/tedlium/TEDLIUM_release-3/data/converted/wav/"
+        self.pattern = "*.wav"
+
+class VoxforgeDataset(Dataset):
+    def __init__(self):
+        self.corpus_name = "voxforge"
+        self.dataset_name = "voxforge"
+        self.audio_dir = "/home/dzubke/awni_speech/data/voxforge/archive/"
+        self.pattern = "*/*/*.wv"
+
+class TatoebaDataset(Dataset):
+    def __init__(self):
+        self.corpus_name = "tatoeba"
+        self.dataset_name = "tatoeba"
+        self.audio_dir = "/home/dzubke/awni_speech/data/tatoeba/tatoeba_audio_eng/audio/"
+        self.pattern = "*/*.wv"
--- tests/pytest_files/noise_inject_test.py
+++ tests/pytest_files/noise_inject_test.py
@@ -5,43 +5,25 @@ import glob
 import pytest
 # project libraries
 from speech.utils import wave, spec_augment, noise_injector, data_helpers
+from speech import dataset_info
 
 def test_datasets():
-    dataset_dir = {
-        "train-clean-100":{
-             "path": "/home/dzubke/awni_speech/data/LibriSpeech/train-clean-100",
-             "pattern":"*/*/*.wav"},
-        "train-clean-360":{
-            "path": "/home/dzubke/awni_speech/data/LibriSpeech/train-clean-360",
-            "pattern":"*/*/*.wav"},
-        "train-other-500":{
-            "path": "/home/dzubke/awni_speech/data/LibriSpeech/train-clean-500",
-            "pattern":"*/*/*.wav"},
-        "tedlium":{
-            "path": "/home/dzubke/awni_speech/data/tedlium/TEDLIUM_release-3/data/converted/wav/",
-            "pattern":"*.wav"},
-        "voxforge":{
-            "path": "/home/dzubke/awni_speech/data/voxforge/archive/",
-            "pattern":"*/*/*.wv"},
-        "tatoeba":{
-            "path": "/home/dzubke/awni_speech/data/tatoeba/tatoeba_audio_eng/audio/",
-            "pattern":"*/*.wv"}
-    }
-        
-    dataset_name = "tatoeba"
-    dataset = dataset_dir[dataset_name]
-    audio_pattern = os.path.join(dataset["path"], dataset["pattern"])
+    dataset_name = "Tatoeba"
+    # initializing the dataset object specified by dataset_name
+    dataset = eval("dataset_info."+dataset_name+"Dataset")()   
+    audio_pattern = os.path.join(dataset.audio_dir, dataset.pattern)
     audio_files = glob.glob(audio_pattern)
     noise_path = "/home/dzubke/awni_speech/data/background_noise/388338__uminari__short-walk-thru-a-noisy-street-in-a-mexico-city.wav"
     for audio_file in audio_files:
         #check_all_noise(audio_file)
-        if data_helpers.skip_file(dataset_name, audio_file):
+        if data_helpers.skip_file(dataset.corpus_name, audio_file):
             print(f"skipping: {audio_file}")
             continue            
         try:
             check_length(audio_file, noise_path)
         except AssertionError:
             raise AssertionError(f"error in audio: {audio_file} and noise: {noise_path}")
+            
 def check_all_noise(audio_path):
     noise_dir = "/home/dzubke/awni_speech/data/background_noise/"
     pattern = "*.wav"
