f30ad0f3262af28f8acf0d364edd582948efe4d7
Dustin Zubke <dzubke@outlook.com>
2020-04-22 14:01:36 -0600

    added speed_vol_perturb and tests

---
+++ speech/utils/speed_vol_perturb.py
@@ -0,0 +1,43 @@
+# standard libraries
+from tempfile import NamedTemporaryFile
+import os
+# third party libraries
+import numpy as np
+# project libraries
+from speech.utils.wave import array_from_wave
+
+"""
+This code has been taken from Sean Naren's Deepspeech implementation at:
+https://github.com/SeanNaren/deepspeech.pytorch/blob/master/data/data_loader.py
+It uses the sox utility to change the speed (tempo) and volume (gain)
+of an input file by path.
+"""
+
+
+def augment_audio_with_sox(path, sample_rate, tempo, gain):
+    """
+    Changes tempo and gain of the recording with sox and loads it.
+    """
+    with NamedTemporaryFile(suffix=".wav") as augmented_file:
+        augmented_filename = augmented_file.name
+        sox_augment_params = ["tempo", "{:.3f}".format(tempo), "gain", "{:.3f}".format(gain)]
+        sox_params = "sox \"{}\" -r {} -c 1 -b 16 -e si {} {} >/dev/null 2>&1".format(path, sample_rate,
+                                                                                      augmented_filename,
+                                                                                      " ".join(sox_augment_params))
+        os.system(sox_params)
+        data, samp_rate = array_from_wave(augmented_filename)
+        return data
+
+def speed_vol_perturb(path, sample_rate=16000, tempo_range=(0.85, 1.15),
+                                  gain_range=(-6, 8)):
+    """
+    Picks tempo and gain uniformly, applies it to the utterance by using sox utility.
+    Returns the augmented utterance.
+    """
+    low_tempo, high_tempo = tempo_range
+    tempo_value = np.random.uniform(low=low_tempo, high=high_tempo)
+    low_gain, high_gain = gain_range
+    gain_value = np.random.uniform(low=low_gain, high=high_gain)
+    audio = augment_audio_with_sox(path=path, sample_rate=sample_rate,
+                                   tempo=tempo_value, gain=gain_value)
+    return audio
