987d68cb85483f88c28951f64ebc3065347b58fb
Dustin <dzubke@outlook.com>
2020-04-27 15:22:57 -0600

    2020-04-27 model updated from ph-2

--- tests/pytest/test_preproc.py
+++ tests/pytest/test_preproc.py
@@ -5,11 +5,11 @@ from speech.loader import Preprocessor, AudioDataset
 from speech.utils.config import Config
 
 def test_main():
-    config_json = ""
+    config_json = "./cv-val_ctc-config.json"
     run_preprocess(config_json)
 
 
-def run_preprocess(config_jsjon:str):
+def run_preprocess(config_json:str):
     """
     Runs the preprocess methood in the Preprocessor object
     over the dataset specified in config_json
@@ -19,18 +19,20 @@ def run_preprocess(config_jsjon:str):
     logger = None
     preproc = Preprocessor(data_json, config.preproc_cfg, logger)
     audio_dataset=AudioDataset(data_json, preproc, batch_size=8)
-
+    
+    index_count = 0
     for index in range(len(audio_dataset.data)):
-        print(audio_dataset[index])
-        assert audio_dataset[index] == True
+        audio_dataset[index]
+        index_count += 1
+    assert index_count == len(audio_dataset.data)
 
-def pytest_addoption(parser):
-    parser.addoption("--json_path", type=str, 
-         help="A json file of a dataset.")
+#def pytest_addoption(parser):
+#    parser.addoption("--json_path", type=str, 
+#         help="A json file of a dataset.")
 
-def pytest_generate_tests(metafunc):
-    # This is called for every test. Only get/set command line arguments
-    # if the argument is specified in the list of test "fixturenames".
-    option_value = metafunc.config.option.name
-    if 'name' in metafunc.fixturenames and option_value is not None:
-        metafunc.parametrize("name", [option_value])
+#def pytest_generate_tests(metafunc):
+#    # This is called for every test. Only get/set command line arguments
+#    # if the argument is specified in the list of test "fixturenames".
+#    option_value = metafunc.config.option.name
+#    if 'name' in metafunc.fixturenames and option_value is not None:
+#        metafunc.parametrize("name", [option_value])
