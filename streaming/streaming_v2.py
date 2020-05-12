fffd8c9a4d07a28458a17fb3ecd785fb4a8e41e3
Dustin Zubke <dzubke@outlook.com>
2020-05-12 16:55:34 -0600

    resolving conflicts after rename/merge troubles

--- streaming/streaming_v2.py
+++ streaming/streaming_v2.py
@@ -21,39 +21,17 @@ import editdistance
 from speech.loader import log_specgram_from_file
 from speech.models.ctc_decoder import decode as ctc_decode
 import speech.models as models
-<<<<<<< HEAD
 from model_convert.get_paths import validation_paths
 from model_convert.import_export import preproc_to_dict, preproc_to_json, export_state_dict
 from model_convert.import_export import torch_onnx_export, onnx_coreml_export
 from model_convert.get_test_input import generate_test_input
 
 
-=======
-from get_paths import validation_paths
-from import_export import preproc_to_dict, preproc_to_json, export_state_dict
-from get_test_input import generate_test_input
-from import_export import torch_onnx_export, onnx_coreml_export
-
-
-
-"""
-CONFIG_FN = '/Users/dustin/CS/consulting/firstlayerai/phoneme_classification/src/awni_speech/speech/onnx_coreml/validation_scripts/ctc_config_20200121-0127.json'
-TRAINED_MODEL_FN = '/Users/dustin/CS/consulting/firstlayerai/phoneme_classification/src/awni_speech/speech/onnx_coreml/torch_models/20200121-0127_best_model_pyt14.pth'
-STATE_DICT_FN = '/Users/dustin/CS/consulting/firstlayerai/phoneme_classification/src/awni_speech/speech/onnx_coreml/validation_scripts/state_params_20200121-0127.pth'
-ONNX_FN = '/Users/dustin/CS/consulting/firstlayerai/phoneme_classification/src/awni_speech/speech/onnx_coreml/onnx_models/20200121-0127_best_model_pyt14.onnx'
-COREML_FN = '/Users/dustin/CS/consulting/firstlayerai/phoneme_classification/src/awni_speech/speech/onnx_coreml/coreml_models/20200121-0127_best_model_pyt14.mlmodel'
-PREPROC_FN = '/Users/dustin/CS/consulting/firstlayerai/phoneme_classification/src/awni_speech/speech/onnx_coreml/preproc/20200121-0127_best_preproc.pyc'
-"""
-
->>>>>>> b7447885fcb6d0d9d8f68ae96baac7207972f79d
 np.random.seed(2020)
 torch.manual_seed(2020)
 
 freq_dim = 257 #freq dimension out of log_spectrogram 
-<<<<<<< HEAD
 label_dim = 39 # 39 phoneme labels
-=======
->>>>>>> b7447885fcb6d0d9d8f68ae96baac7207972f79d
 
 def main(model_name, num_frames):
 
@@ -67,7 +45,6 @@ def main(model_name, num_frames):
         config = json.load(fid)
         model_cfg = config["model"]
 
-<<<<<<< HEAD
     #loading models
     state_dict_model = torch.load(TRAINED_MODEL_FN, map_location=torch.device('cpu'))
     print("======= state_dict_model =======\n", state_dict_model)
@@ -75,39 +52,16 @@ def main(model_name, num_frames):
     trained_model = models.CTC(freq_dim, label_dim, model_cfg)
     print("======= trained_model =======\n", trained_model)
 
-=======
-    #load models
-    state_dict_model = torch.load(TRAINED_MODEL_FN, map_location=torch.device('cpu'))
-
-    trained_model = models.CTC(freq_dim,40, model_cfg)
->>>>>>> b7447885fcb6d0d9d8f68ae96baac7207972f79d
     state_dict = state_dict_model.state_dict()
     torch.save(state_dict, state_dict_path)
     trained_model.load_state_dict(state_dict)
 
-<<<<<<< HEAD
     onnx_model = onnx.load(ONNX_FN)
 
     coreml_model = coremltools.models.MLModel(COREML_FN)
 
     # prepping and checking models
     trained_model.eval()
-=======
-    #exporting and loading onnx model
-    #torch_device = 'cpu'
-    #input_tensor = generate_test_input("pytorch", model_name, time_dim=time_dim, set_device=torch_device) 
-    #torch_onnx_export(trained_model, input_tensor, onnx_path)
-    onnx_model = onnx.load(ONNX_FN)
-
-    # exporting and loading coreml model
-    #onnx_coreml_export(onnx_path, coreml_path)
-    coreml_model = coremltools.models.MLModel(COREML_FN)
-
-
-    # prepping and checking models
-    trained_model.eval()
-    ##CTCNet_model.eval()
->>>>>>> b7447885fcb6d0d9d8f68ae96baac7207972f79d
 
     onnx.checker.check_model(onnx_model)
     inferred_model = shape_inference.infer_shapes(onnx_model)
@@ -126,7 +80,6 @@ def main(model_name, num_frames):
 
         trained_output = trained_model(torch.from_numpy(test_x),(torch.from_numpy(test_h), torch.from_numpy(test_c))) 
         trained_probs, trained_h, trained_c = to_numpy(trained_output[0]), to_numpy(trained_output[1][0]), to_numpy(trained_output[1][1])
-<<<<<<< HEAD
         trained_max_decoder = max_decode(trained_probs[0], blank=39)
         trained_ctc_decoder = ctc_decode(trained_probs[0], beam_size=50, blank=39)
 
@@ -169,94 +122,6 @@ def main(model_name, num_frames):
             #print(f"probs_full:{probs_slice.shape} {probs_slice[0,0,:]}")
             stream_probs = to_numpy(stream_probs)
             #print(f"probs_chunk:{probs_chunk.shape} {probs_chunk[0,0,:]}")
-=======
-        trained_max_decoder = max_decode(trained_probs[0], blank=40)
-        trained_ctc_decoder = ctc_decode(trained_probs[0], beam_size=50, blank=40)
-        trained_max_decoder_char = ints_to_phonemes(PREPROC_FN, trained_max_decoder)
-        trained_ctc_decoder_char = ints_to_phonemes(PREPROC_FN, trained_ctc_decoder[0])
-        print("torch prediction complete") 
-        print(f"input size: {test_x.shape}")
-
-        # ort_session = onnxruntime.InferenceSession(ONNX_FN)
-        # ort_inputs = {
-        # ort_session.get_inputs()[0].name: test_x,
-        # ort_session.get_inputs()[1].name: test_h,
-        # ort_session.get_inputs()[2].name: test_c}
-        # ort_output = ort_session.run(None, ort_inputs)
-        # onnx_probs, onnx_h, onnx_c = [np.array(array) for array in ort_output]
-        # print("onnxruntime prediction complete") 
-
-        # coreml_input = {'input': test_x, 'hidden_prev': test_h, 'cell_prev': test_c}
-        # coreml_output = coreml_model.predict(coreml_input, useCPUOnly=True)
-        # coreml_probs = np.array(coreml_output['output'])
-        # coreml_h = np.array(coreml_output['hidden'])
-        # coreml_c = np.array(coreml_output['cell'])
-        # coreml_max_decoder = max_decode(coreml_probs[0], blank=40)
-        # coreml_ctc_decoder = ctc_decode(coreml_probs[0], beam_size=50,blank=40)
-        # print("coreml prediction completed")
-
-        # # Compare Trained and Coreml predictions
-        # np.testing.assert_allclose(coreml_probs, trained_probs, rtol=1e-03, atol=1e-05)
-        # np.testing.assert_allclose(coreml_h, trained_h, rtol=1e-03, atol=1e-05)
-        # np.testing.assert_allclose(coreml_c, trained_c, rtol=1e-03, atol=1e-05)
-        # #assert(trained_max_decoder==coreml_max_decoder), "max decoder doesn't match"
-        # #assert(trained_ctc_decoder[0]==coreml_ctc_decoder[0]), "ctc decoder labels don't match"
-        # #np.testing.assert_almost_equal(trained_ctc_decoder[1], coreml_ctc_decoder[1], decimal=3)
-        # print("\nTrained and Coreml probs, hidden, cell states match, all good!")
-    
-        print("\n\n------------- Streaming Validation --------------")
-
-        stream_test_x = test_x
-        stream_test_probs = trained_probs
-        stream_test_h_out = trained_h
-        stream_test_c_out = trained_c
-        stream_test_max_decoder = trained_max_decoder
-        stream_test_ctc_decoder = trained_ctc_decoder
-
-        context_size = 31
-        dist_dict = {}
-
-        #initializing the hidden and cell states for the coreml model
-        coreml_h_in = test_h
-        coreml_c_in = test_c
-
-        #initializing the hidden and cell states for the torch model
-        stream_test_h_in = stream_test_h_out = torch.from_numpy(test_h)
-        stream_test_c_in = stream_test_c_out = torch.from_numpy(test_c)
-        for i in range(217):
-            print(f"outer loop: {i}")
-
-
-            input_buffer = test_x[:, i:i+context_size, :]
-
-            np.testing.assert_allclose(to_numpy(stream_test_h_in), to_numpy(stream_test_h_out), rtol=1e-03, atol=1e-05)
-            np.testing.assert_allclose(to_numpy(stream_test_c_in), to_numpy(stream_test_c_out), rtol=1e-03, atol=1e-05)
-            print(f"input_buffer size: {input_buffer.shape}")
-            stream_probs, (stream_test_h_out, stream_test_c_out) = trained_model(torch.from_numpy(input_buffer), 
-                                        (stream_test_h_in,
-                                        stream_test_c_in)
-                                        )
-            stream_test_h_in = stream_test_h_out
-            stream_test_c_in = stream_test_c_out
-
-            
-            coreml_input = {'input': input_buffer, 'hidden_prev': coreml_h_in, 'cell_prev': coreml_c_in}
-            coreml_output = coreml_model.predict(coreml_input, useCPUOnly=True)
-            coreml_stream_probs = np.array(coreml_output['output'])
-            coreml_h_in = np.array(coreml_output['hidden'])
-            coreml_c_in = np.array(coreml_output['cell'])
-
-
-            print(f"stream_probs: {stream_probs.shape} \n {stream_probs}")
-
-
-            #probs_slice = stream_test_probs[:, int(i*stream_step/2) : int((i+1)*stream_step/2),:]
-            #print(f"probs_full:{probs_slice.shape} {probs_slice[0,0,:]}")
-
-            stream_probs = to_numpy(stream_probs)
-            #print(f"probs_chunk:{probs_chunk.shape} {probs_chunk[0,0,:]}")
-            
->>>>>>> b7447885fcb6d0d9d8f68ae96baac7207972f79d
             if i ==0:
                 stream_output = stream_probs
                 coreml_probs = coreml_stream_probs
@@ -266,25 +131,12 @@ def main(model_name, num_frames):
 
         np.testing.assert_allclose(stream_output, trained_probs, rtol=1e-3, atol=1e-3)
         np.testing.assert_allclose(coreml_probs, trained_probs, rtol=1e-3, atol=1e-3)
-<<<<<<< HEAD
         print("the outputs are the same")
 
         stream_max_decoded = ints_to_phonemes(PREPROC_FN, max_decode(stream_output[0], blank=39))
         full_max_decoder = ints_to_phonemes(PREPROC_FN, stream_test_max_decoder)
         print("non_stream labels: ", full_max_decoder)
         print("stream labels: ", stream_max_decoded)
-=======
-
-        print("the outputs are the same")
-
-        #print(f"probs_chunk size: {probs_chunks.shape}")
-        #full_max_decoder = ints_to_phonemes(PREPROC_FN, stream_test_max_decoder)
-        #chunked_max_decoder = ints_to_phonemes(PREPROC_FN,max_decode(np.squeeze(probs_chunks), blank=40))
-
-        #dist_dict.update({stream_step: editdistance.eval(full_max_decoder, chunked_max_decoder)})
-        #print(dist_dict)
-        
->>>>>>> b7447885fcb6d0d9d8f68ae96baac7207972f79d
 
 
 def normalize(PREPROC_FN, inputs):
@@ -297,10 +149,6 @@ def ints_to_phonemes(PREPROC_FN, int_list):
         preproc = pickle.load(fid)
         return preproc.decode(int_list)
 
-<<<<<<< HEAD
-=======
-
->>>>>>> b7447885fcb6d0d9d8f68ae96baac7207972f79d
 def gen_test_data(preproc_path, time_dim, freq_dim):
     test_x_zeros = np.zeros((1, time_dim, freq_dim)).astype(np.float32)
     test_h_zeros = np.zeros((5, 1, 512)).astype(np.float32)
@@ -329,32 +177,20 @@ def gen_test_data(preproc_path, time_dim, freq_dim):
                 "ST-58cynYij95TbB9Nlz3TrKBbkg643-1574725017.wv", "ST-CcSEvcOEineimGwKOk1c8P2eU0q1-1574725123.wv", 
                 "ST-OVrsxD1n9Wbh0Hh6thej8FIBIOE2-1574725033.wv", "ST-R3SdlQCwoYQkost3snFxzXS5vam2-1574726165.wv"]
                           
-<<<<<<< HEAD
     base_path = '/Users/dustin/CS/consulting/firstlayerai/phoneme_classification/src/awni_speech/speech/model_convert/audio_files/'
-=======
-    base_path = './audio_files/'
->>>>>>> b7447885fcb6d0d9d8f68ae96baac7207972f79d
     audio_dct = load_audio(preproc_path, test_names, test_fns, base_path, test_h_zeros, test_c_zeros, time_dim)
     test_dct = {} #{'test_zeros': test_zeros, 'test_randn_seed-2020': test_randn}
     test_dct.update(audio_dct)
 
     return test_dct
 
-<<<<<<< HEAD
-=======
-
->>>>>>> b7447885fcb6d0d9d8f68ae96baac7207972f79d
 def load_audio(preproc_path, test_names, test_fns, base_path, test_h, test_c, time_dim):
     dct = {}
     for test_name, test_fn in zip(test_names, test_fns):
 
         audio_data = normalize(preproc_path, log_specgram_from_file(base_path+test_fn))
         #audio_data = audio_data[:time_dim,:]       #concatenates the audio_data after time_dim
-<<<<<<< HEAD
         audio_data = np.expand_dims(audio_data, axis=0)
-=======
-        audio_data = np.expand_dims(audio_data, 0)
->>>>>>> b7447885fcb6d0d9d8f68ae96baac7207972f79d
         dct.update({test_name : [audio_data, test_h, test_c]})
     return dct
 
@@ -474,11 +310,7 @@ def to_numpy(tensor):
         return tensor.detach().cpu().numpy() if tensor.requires_grad else tensor.cpu().numpy()
 
 
-<<<<<<< HEAD
 def max_decode(output, blank=39):
-=======
-def max_decode(output, blank=40):
->>>>>>> b7447885fcb6d0d9d8f68ae96baac7207972f79d
     pred = np.argmax(output, 1)
     prev = pred[0]
     seq = [prev] if prev != blank else []
