6cffc6f0e4ba6f2135b84dbf22de67ef6e474cf3
Dustin Zubke <dzubke@outlook.com>
2020-06-22 15:19:44 -0600

    syncing local

---
+++ tests/pytest/preprocessor/test_compute_mean_std.py
@@ -0,0 +1,12 @@
+# third-party libraries
+import numpy as np
+import pytest
+# project libraries
+from speech.loader import feature_normalize, log_spectrogram_from_file
+from tests.pytest.utils import get_all_test_audio
+
+
+
+# def test_compute_mean_std():
+
+#     audio_files = get_all_test_audio()
