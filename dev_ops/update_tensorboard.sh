55afbdb58c9cc250c45b6f53bedb26bd2e14354c
Dustin <dzubke@outlook.com>
2020-07-10 20:05:51 +0000

    corrected avg_loss and log to tensorboard

---
+++ dev_ops/update_tensorboard.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# usage: bash updated_tensorboard.sh
+
+# description: will updated the tensorboard in the awni_env36 environment on July 10 2020
+
+pip uninstall -y tensorflow-tensorboard
+pip uninstall -y tensorboard-logger
+conda remove -y tensorflow-base
+pip install tensorflow==1.15.3
+pip install -U tensorboardX
